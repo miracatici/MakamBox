23926299f32b364a9c2ac7f97500512082cbe4ed
Bilge Mirac Atici <miracatici@gmail.com>
2015-09-27 00:07:25 +0300

    Added

--- src/test/VampTester.java
+++ src/test/VampTester.java
@@ -2,7 +2,7 @@ package test;
 
 public class VampTester {
 	public static void main (String[] arg){		
-		String airair1;
+		String propro2;
 //		Plugin p = null;
 //		try {
 //			p = PluginLoader.getInstance().loadPlugin("mtg-melodia:melodia", 44100,PluginLoader.AdapterFlags.ADAPT_NONE);
