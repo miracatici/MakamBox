2239dc1c893c537bedb48d49c6c2005afec3224b
Bilge Mirac Atici <miracatici@gmail.com>
2016-01-02 23:03:56 +0200

    proJSON

---
+++ src/test/JSON.java
@@ -0,0 +1,11 @@
+package test;
+
+import javax.json.Json;
+import javax.json.JsonObject;
+
+public class JSON {
+
+	public static void main(String[] args) {
+		JsonObject object = Json.createObjectBuilder().build();
+	}
+}
